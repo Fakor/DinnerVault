e8f48a3ab819947ef11982882ff7c1eabab653cb
david <davidjohansson35@gmail.com>
2019-12-03 22:33:22 +0100

    Classification added to meal

--- glufs/main/models.py
+++ glufs/main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class Classification(models.Model):
+    name = models.CharField(max_length=10)
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=50)
     grade = models.IntegerField(null=True)
+    classification = models.ForeignKey(Classification, on_delete=models.PROTECT, null=False)
