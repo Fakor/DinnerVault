fe27e630c4a5b5257184677010240125f8e2397b
david <davidjohansson35@gmail.com>
2019-12-04 22:36:47 +0100

    overview view

--- glufs/main/views.py
+++ glufs/main/views.py
@@ -1,8 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Meal
 
 
 def index(request):
     return HttpResponse("Glufs!")
+
+
+def overview(request):
+    context={'meals': Meal.objects.all()}
+    return render(request, 'main/overview.html', context)
