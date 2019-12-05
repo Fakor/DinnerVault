c516efeaa03404be6863b1ef15587ee10c559f0c
david <davidjohansson35@gmail.com>
2019-12-05 22:24:52 +0100

    Recipe and note added to meal

--- glufs/main/models.py
+++ glufs/main/models.py
@@ -5,7 +5,14 @@ class Classification(models.Model):
     name = models.CharField(max_length=10)
 
 
+class Note(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=50)
     grade = models.IntegerField(null=True)
     classification = models.ForeignKey(Classification, on_delete=models.PROTECT, null=False)
+    recipe = models.CharField(max_length=1000, null=True)
+    notes = models.ManyToManyField(Note)
