40921b8aa19ab744578981c816767de685fc7a97
david <davidjohansson35@gmail.com>
2019-12-27 22:51:14 +0100

    Ingredients part of Meal

--- glufs/main/models.py
+++ glufs/main/models.py
@@ -10,12 +10,19 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=20, null=True)
+
+
 class Meal(models.Model):
     name = models.CharField(max_length=50)
     grade = models.IntegerField(null=True)
     classification = models.ForeignKey(Classification, on_delete=models.PROTECT, null=False)
     recipe = models.CharField(max_length=1000, null=True)
     notes = models.ManyToManyField(Note)
+    ingredients = models.ManyToManyField(Ingredient)
 
     def add_note(self, text):
         note = Note(text=text)
