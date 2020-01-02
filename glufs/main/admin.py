7c2f7a775b866d9b9adbefc168792f8fda19828e
david <davidjohansson35@gmail.com>
2020-01-02 21:11:58 +0100

    Some admin stuff added

--- glufs/main/admin.py
+++ glufs/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Meal
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Meal, MealAdmin)
