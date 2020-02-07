7067515331ec1c5ab2d66bf18e9fd9f8df57d0c9
david <davidjohansson35@gmail.com>
2020-02-07 21:10:53 +0100

    Top button style added

--- src/my_statics/main/js/base.js
+++ src/my_statics/main/js/base.js
@@ -6,3 +6,12 @@ function create_header(path) {
     header.appendChild(img)
     document.body.appendChild(header);
 }
+
+function create_top_button(text, path){
+    let button=document.createElement("button");
+    button.setAttribute("class", "top");
+    button.onclick= function() {location.href=path;};
+    button.setAttribute("type", "button");
+    button.innerHTML = text;
+    document.body.appendChild(button);
+}
