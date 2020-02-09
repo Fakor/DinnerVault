a68fc8fc655f8aec30fcbdc3d69975937ff6871f
david <davidjohansson35@gmail.com>
2020-02-09 07:38:14 +0100

    CreateTopButton->AddHeaderButton

--- src/my_statics/main/js/base.js
+++ src/my_statics/main/js/base.js
@@ -1,17 +1,21 @@
 function create_header(path) {
     let header=document.createElement("div")
     header.setAttribute("class", "header");
+    header.setAttribute("id", "header");
     img = document.createElement("img");
     img.setAttribute("src", path);
     header.appendChild(img)
+    header.innerHTML+= "<br>"
     document.body.appendChild(header);
 }
 
-function create_top_button(text, path){
+function add_header_button(text, path){
     let button=document.createElement("button");
     button.setAttribute("class", "top");
     button.onclick= function() {location.href=path;};
     button.setAttribute("type", "button");
     button.innerHTML = text;
-    document.body.appendChild(button);
+    button.setAttribute("align" ,"right");
+    header = document.getElementById("header");
+    header.append(button);
 }
