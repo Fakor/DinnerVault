6b9f046920d62b076853c3355a30c942d62c384c
david <davidjohansson35@gmail.com>
2020-02-15 20:40:15 +0100

    A title in the header for each page

--- src/my_statics/main/js/base.js
+++ src/my_statics/main/js/base.js
@@ -1,4 +1,4 @@
-function create_header(path) {
+function create_header(path, title) {
     let header=document.createElement("div")
     header.setAttribute("class", "header");
     header.setAttribute("id", "header");
@@ -17,13 +17,18 @@ function create_header(path) {
     header.appendChild(img);
     header.appendChild(l_div);
 
+
     document.body.appendChild(header);
 
     add_header_button("Overview", "/main/overview");
     add_header_button("Create dinner", "/main/create");
     add_header_button("Create label", "/main/create_label");
 
-    add_header_button_right("Logout", "/logout")
+    let set_title=document.createElement("h2");
+    set_title.innerHTML=title;
+    l_div.append(set_title);
+
+    add_header_button_right("Logout", "/logout");
 }
 
 function add_header_button(text, path){
