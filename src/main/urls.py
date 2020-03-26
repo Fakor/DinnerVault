0eaafec9a0ee06e4691e235363ffd5f1c038013b
david <davidjohansson35@gmail.com>
2020-03-26 21:56:04 +0100

    Refactoring, ViewEditMeal

--- src/main/urls.py
+++ src/main/urls.py
@@ -7,13 +7,14 @@ from django.contrib.auth.decorators import login_required
 from main.views.overview import ViewOverview
 from main.views.detail import ViewDetail
 from main.views.create_meal import ViewCreateMeal
+from main.views.edit_meal import ViewEditMeal
 
 urlpatterns = [
 #    url(r'^$', views.index, name='index'),
     path('overview/', login_required(ViewOverview.as_view()), name='overview'),
     path('detail/<int:meal_id>/', login_required(ViewDetail.as_view()), name='detail'),
     path('create/', login_required(ViewCreateMeal.as_view()), name='create_meal'),
-#    path('edit_meal/<int:meal_id>/', views.edit_meal, name='edit_meal'),
+    path('edit_meal/<int:meal_id>/', login_required(ViewEditMeal.as_view()), name='edit_meal'),
 #    path('create_label/', views.create_label, name='create_label'),
 #    path('edit_label/<int:label_id>/', views.edit_label, name='edit_label'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ src/main/views/edit_meal.py
@@ -0,0 +1,39 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404
+
+from main.models import Meal
+from main.forms import EditMealForm, LabelPickerForm
+
+
+class ViewEditMeal(View):
+    template_name='main/edit_meal.html'
+    detail_name = 'detail'
+
+    def get(self, request, meal_id):
+        return render(request, self.template_name, self.get_context(meal_id))
+
+    def post(self, request, meal_id):
+        meal = get_object_or_404(Meal, pk=meal_id)
+        form = EditMealForm(request.POST)
+        form_labels = LabelPickerForm(request.POST)
+        if 'delete_dates' in request.POST:
+            for date_id in request.POST.getlist('dates'):
+                d = meal.dates.filter(id=int(date_id))
+                d.delete()
+        elif form.is_valid() and form_labels.is_valid():
+            form.update_meal(meal)
+            form_labels.update_meal_with_labels(meal)
+            return redirect(self.detail_name, meal_id=(meal.id))
+        return render(request, self.template_name, self.get_context(meal_id))
+
+    def get_context(self, meal_id):
+        meal = get_object_or_404(Meal, pk=meal_id)
+        form = EditMealForm(instance=meal)
+        form_labels = LabelPickerForm(labels=meal.labels)
+
+        return {'form': form,
+                'new': False,
+                'meal': meal,
+                'form_labels': form_labels,
+                'dates': meal.dates.all() }
